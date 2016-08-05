9f09b5d01e006fa4d70777faefc087c7c86a9bb9
satub <satu.barnhill@gmail.com>
2016-08-05 17:46:21 -0400

    Done

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
+  def change #do this, and then undo it on rollback.
+    #ActiveRecord creates id PRIMARY KEY column automatically
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
